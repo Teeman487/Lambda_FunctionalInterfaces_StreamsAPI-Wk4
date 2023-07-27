a9995208a33dd3c604cb25f6fca7bf83b32172b3
Akinade Toheeb Adebusoye <adebusoyeteeman@gmail.com>
2023-07-26 23:21:10 -1100

    T

---
+++ src/Imperative/_Function.java
@@ -0,0 +1,16 @@
+package Imperative;
+
+public class _Function {
+    public static void main(String[] args) {
+        int increment = increment(1);
+        System.out.println(increment);
+    }
+
+        static int increment(int number){
+            return number + 1;
+        }
+
+
+
+
+    }
