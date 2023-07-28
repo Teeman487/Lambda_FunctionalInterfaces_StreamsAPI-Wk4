61e6b1d5ad325ed30a141538647242ef13146818
Akinade Toheeb Adebusoye <adebusoyeteeman@gmail.com>
2023-07-28 05:44:55 -1100

    Toheeb

--- src/Imperative/_Function.java
+++ src/Imperative/_Function.java
@@ -1,12 +1,21 @@
 package Imperative;
 
+import java.util.function.Function;
+
 public class _Function {
     public static void main(String[] args) {
-        int increment = increment(1);
+        int increment = incrementByOne(1);
         System.out.println(increment);
+
+      int increment2= incrementByOneFunction.apply(1);
+        System.out.println(increment2);
+
     }
 
-        static int increment(int number){
+    static Function<Integer, Integer> incrementByOneFunction =
+            number -> number + 1;
+
+        static int incrementByOne(int number){
             return number + 1;
         }
 
